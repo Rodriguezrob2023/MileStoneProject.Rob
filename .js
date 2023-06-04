ddd2199927e37632c895742f506eabd70e8e14a4
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-06-04 12:32:12 -0700

    Changes to Javascript update.

---
+++ .js
@@ -0,0 +1,50 @@
+let questions = [
+{
+question: "Inside which HTML element do we put the JavaScript?",
+  answers: [
+    {text: "<js>", correct: False},
+    {text: "<script>", correct: True},
+    {text: "<scripting>", correct: False},
+    {text: "<javascript>", correct: False},
+]
+},
+{
+    question: "How do you write "Hello World" in an alert box?" ,
+  answers: [
+    {text: "alert("Hello World")", correct: True},
+    {text: "msgBox("Hello World")", correct: False},
+    {text: "alerBox("Hello World")", correct: False},
+    {text: "msg("Hello World")", correct: False},
+]
+},
+{
+    question: "How do you call a function named "myFunction"?" ,
+    answers: [
+      {text: "call myFunction()", correct: False},
+      {text: "call function myFunction()", correct: False},
+      {text: "myFunction()", correct: True},
+      {text: "Whatsyourfunction()", correct: False},
+  ]
+
+
+
+},
+{
+    {
+        question: "Which operator is used to assign a value to a variable"?", 
+        answers: [
+          {text: "-", correct: False},
+          {text: "+", correct: False},
+          {text: ";]", correct: False},
+          {text: "=", correct: True},
+      ]
+    
+    
+    
+    }
+
+
+
+
+}
+]
